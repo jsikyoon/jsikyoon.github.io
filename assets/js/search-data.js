5b4fbbb6546aad8a400759240800e6069f2b49a0
jsikyoon <mail@jaesikyoon.com>
2025-06-18 07:29:29 +0000

    Deploying to gh-pages from @ jsikyoon/jsikyoon.github.io@ec25c1687c3ac66427fcdd17c3fb880712e38de9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,12 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-fast-monte-carlo-tree-diffusion-100x-speedup-via-parallel-sparse-planning-is-released-on-arxiv",
           title: 'Our paper Fast Monte Carlo Tree Diffusion: 100x Speedup via Parallel Sparse Planning...',
           description: "",
-          section: "News",},{id: "projects-monte-carlo-tree-diffusion-for-system-2-planning",
+          section: "News",},{id: "projects-fast-monte-carlo-tree-diffusion-100x-speedup-via-parallel-sparse-planning",
+          title: 'Fast Monte Carlo Tree Diffusion: 100x Speedup via Parallel Sparse Planning',
+          description: "A novel approach to optimize the efficiency of Monte Carlo Tree Diffusion",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/fast-mctd/";
+            },},{id: "projects-monte-carlo-tree-diffusion-for-system-2-planning",
           title: 'Monte Carlo Tree Diffusion for System 2 Planning',
           description: "A novel approach blending Monte Carlo Tree Search and Diffusion Models",
           section: "Projects",handler: () => {
